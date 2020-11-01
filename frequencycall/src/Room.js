1571aa8878f867dd5fbb5e5bd0b4f9104fc7c779
Sokos6 <will.sokolowski@gmail.com>
2020-11-01 14:10:46 -0600

    working on room

--- frequencycall/src/Room.js
+++ frequencycall/src/Room.js
@@ -1,9 +1,18 @@
 import { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
-import Peer from 'simple-peer'
+import Peer from 'simple-peer';
 import * as Chance from 'chance';
 import Video from './Video';
 
 const chance = new Chance();
+
+const Room = (props) => {
+  return (
+    <div>
+      <h2>TEST</h2>
+      <div>ROOM</div>
+    </div>
+  );
+};
 
 export default Room;
